3de8bb5c6ac7894dd5f3f6ca31adfca43712c70d
Sarthak Mohanty <sarthaktexas@gmail.com>
2021-06-17 01:24:26 -0500

    reduces text size on landing

--- pages/index.js
+++ pages/index.js
@@ -22,7 +22,7 @@ see the source: https://github.com/hackclub/hackfoundation.org
       <HeadObject />
       <main className="m-10 md:mx-12 lg:mx-24 md:my-2">
         <section className="flex flex-col md:grid md:grid-cols-3">
-            <section className="md:mr-16 md:col-span-2 flex flex-col space-y-12 text-lg lg:text-2xl">
+            <section id="main" className="md:mr-16 md:col-span-2 flex flex-col space-y-12 text-lg lg:text-xl">
               <p>
                 <b>The Hack Foundation</b> was founded in 2016 to serve as the legal entity for <a href="https://hackclub.com" target="_blank">Hack Club</a>, a nonprofit network of high schoolers creating the coding education they wish they could receive.
               </p>
