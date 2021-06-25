f4cc794ae4c44050a7af7acf6c01432e739e6d94
Sarthak Mohanty <sarthaktexas@gmail.com>
2021-06-25 19:33:22 +0000

    🚀 deploy orpheus flag (thanks @lachlanjc)

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,11 @@
 import '../styles/globals.css'
 import { ThemeProvider } from 'next-themes'
+import Flag from '../components/Flag'
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider defaultTheme="system" attribute="class">
+      <Flag />
       <Component {...pageProps} />
     </ThemeProvider>
   )
