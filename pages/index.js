b18d280aa823d569ccdbb93eaa6cd472ed2f2818
Sarthak Mohanty <sarthaktexas@gmail.com>
2021-06-25 21:51:40 +0000

    🐛 make all anchors blank targets

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ export default function Home() {
   useEffect(() => {
     console.log(`
                __
-              / _) 
+              / _)
      _.----._/ /
     /         /
  __/ (  | (  |
@@ -28,15 +28,15 @@ see the source: https://github.com/hackclub/hackfoundation.org
         color="#ffffff"
       />
       <main className="m-10 md:mx-12 lg:mx-24 md:my-2">
-        <section className="flex flex-col md:grid md:grid-cols-3">
+        <section className="flex flex-col space-y-10 lg:space-y-0 lg:grid lg:grid-cols-3">
             <section id="main" className="md:mr-16 md:col-span-2 flex flex-col space-y-10 text-lg">
               <p>
-                <b>The Hack Foundation</b> was founded in 2016 to serve as the legal entity for <a href="https://hackclub.com" target="_blank">Hack Club</a>, a nonprofit network of high schoolers creating the coding education they wish they could have.
+                <b>The Hack Foundation</b> was founded in 2016 to serve as the legal entity for <a target="_blank" href="https://hackclub.com">Hack Club</a>, a nonprofit network of high schoolers creating the coding education they wish they could have.
               </p>
               <p>
-                Today, The Hack Foundation is the largest <a href="https://en.wikipedia.org/wiki/Fiscal_sponsorship">fiscal sponsor</a> for teen-led projects in the world.</p>
+                Today, The Hack Foundation is the largest <a target="_blank" href="https://en.wikipedia.org/wiki/Fiscal_sponsorship">fiscal sponsor</a> for teen-led projects in the world.</p>
               <p>
-                Hundreds of diverse groups with annual budgets from $100 to $250,000 are powered by The Hack Foundation. These include Hack Clubs, hackathons, Little League teams, community organizations, and <a href="https://bank.hackclub.com/hq/">Hack Club HQ itself</a>. Partnered organizations receive the benefits of 501(c)(3) nonprofit status, administrative and backoffice support, and financial oversight.
+                Hundreds of diverse groups with annual budgets from $100 to $250,000 are powered by The Hack Foundation. These include Hack Clubs, hackathons, Little League teams, community organizations, and <a target="_blank" href="https://bank.hackclub.com/hq/">Hack Club HQ itself</a>. Partnered organizations receive the benefits of 501(c)(3) nonprofit status, administrative and backoffice support, and financial oversight.
               </p>
               <p>
                 The Hack Foundation was founded by a 16 year old. The organization and all its software has been built over the years by teenagers at Hack Club. Today's supporters include...
@@ -44,8 +44,8 @@ see the source: https://github.com/hackclub/hackfoundation.org
               <OrganizationContainer />
             </section>
             <section className="mt-12 md:mt-0 md:col-span-1 flex flex-col space-y-10">
-              <p>If you are considering supporting an organization partnered with The Hack Foundation, contact our <a href="mailto:bank@hackclub.com" className="font-bold">financial operations team.</a></p>
-              <p>If you would like to learn more about partnering with The Hack Foundation for your organization, <a href="https://hackclub.com/bank/" className="font-bold">please see here.</a></p>
+              <p>If you are considering supporting an organization partnered with The Hack Foundation, contact our <a target="_blank" href="mailto:bank@hackclub.com" className="font-bold">financial operations team.</a></p>
+              <p>If you would like to learn more about partnering with The Hack Foundation for your organization, <a target="_blank" href="https://hackclub.com/bank/" className="font-bold">please see here.</a></p>
               <section id="people">
                 <h3 className="font-bold">Board</h3>
                 <div className="flex flex-col space-y-1 items-start">
