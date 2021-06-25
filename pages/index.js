6eccffcf7893d2483d95108ae394a23a6e0dd951
Sarthak Mohanty <sarthaktexas@gmail.com>
2021-06-25 02:13:08 +0000

    💄 fix body changes + target _blanks

--- pages/index.js
+++ pages/index.js
@@ -22,7 +22,7 @@ see the source: https://github.com/hackclub/hackfoundation.org
       <HeadObject />
       <main className="m-10 md:mx-12 lg:mx-24 md:my-2">
         <section className="flex flex-col md:grid md:grid-cols-3">
-            <section id="main" className="md:mr-16 md:col-span-2 flex flex-col space-y-12 text-lg">
+            <section id="main" className="md:mr-16 md:col-span-2 flex flex-col space-y-10 text-lg">
               <p>
                 <b>The Hack Foundation</b> was founded in 2016 to serve as the legal entity for <a href="https://hackclub.com" target="_blank">Hack Club</a>, a nonprofit network of high schoolers creating the coding education they wish they could have.
               </p>
@@ -44,19 +44,19 @@ see the source: https://github.com/hackclub/hackfoundation.org
               <section>
                 <h3 className="font-bold">Board</h3>
                 <section className="flex flex-col space-y-1">
-                  <a href="https://twitter.com/zachlatta">Zach Latta</a>
-                  <a href="https://twitter.com/ChristinaAsquit">Christina Asquith</a>
-                  <a href="https://twitter.com/mojombo">Tom Preston-Werner</a>
-                  <a href="https://twitter.com/sqs">Quinn Slack</a>
+                  <a target="_blank" href="https://twitter.com/zachlatta">Zach Latta</a>
+                  <a target="_blank" href="https://twitter.com/ChristinaAsquit">Christina Asquith</a>
+                  <a target="_blank" href="https://twitter.com/mojombo">Tom Preston-Werner</a>
+                  <a target="_blank" href="https://twitter.com/sqs">Quinn Slack</a>
                 </section>
               </section>
               <section>
                 <h3 className="font-bold">Finances</h3>
                 <section className="flex flex-col space-y-1">
-                  <a href="https://apps.irs.gov/pub/epostcard/cor/812908499_201912_990_2021022417751020.pdf">2019</a>
-                  <a href="https://apps.irs.gov/pub/epostcard/cor/812908499_201812_990_2020013017086607.pdf">2018</a>
-                  <a href="https://apps.irs.gov/pub/epostcard/cor/812908499_201712_990EZ_2019112216882661.pdf">2017</a>
-                  <a href="https://apps.irs.gov/pub/epostcard/cor/812908499_201712_990EZ_2019112216882661.pdf">2016</a>
+                  <a target="_blank" href="https://apps.irs.gov/pub/epostcard/cor/812908499_201912_990_2021022417751020.pdf">2019</a>
+                  <a target="_blank" href="https://apps.irs.gov/pub/epostcard/cor/812908499_201812_990_2020013017086607.pdf">2018</a>
+                  <a target="_blank" href="https://apps.irs.gov/pub/epostcard/cor/812908499_201712_990EZ_2019112216882661.pdf">2017</a>
+                  <a target="_blank" href="https://apps.irs.gov/pub/epostcard/cor/812908499_201712_990EZ_2019112216882661.pdf">2016</a>
                 </section>
               </section>
               <section>
