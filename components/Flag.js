92e5886ff4bc1e6e98844f428b9c3c985e32336e
Sarthak Mohanty <sarthaktexas@gmail.com>
2021-07-13 10:59:43 -0500

    💄 🐛 fix the flag ( @sampoder and @lachlanjc)

--- components/Flag.js
+++ components/Flag.js
@@ -1,40 +1,48 @@
-/**
- * Hack Club Orpheus Flag Tailwind Component.
- * Written by @lachlanjc on hackclub/summer-scrapbook
- * Adapted by @sarthaktexas on hackclub/hackfoundation.org
- */
-
-export default function Flag(props) {
-    return (
-        <a
-            title="Hack Club"
-            href="https://hackclub.com/"
-            className="safe nav-flag absolute top-0 inline-block cursor-pointer flex-shrink-0"
-            {...props}
-        >
-            <img src="https://assets.hackclub.com/flag-orpheus-top.svg" className="pl-5" />
-            <style jsx>{`
-            img {
-                width: 7rem;
-            }
-            @media (prefers-reduced-motion: no-preference) {
-                a {
-                    transition: ${3 / 16}s cubic-bezier(0.375, 0, 0.675, 1) transform;
-                    transform-origin: top left;
-                }
-                a:hover, a:focus {
-                    animation: waveFlag 0.5s linear infinite alternate;
-                }
-            }
-            @keyframes waveFlag {
-                from {
-                    transform: rotate(0deg);
-                }
-                to {
-                    transform: rotate(-5deg);
-                }
-            }
-            `}</style>
-        </a>
-    )
-}
+const Flag = props => (
+    <a
+      title="Hack Club"
+      href="https://hackclub.com/"
+      className="nav-flag"
+      {...props}
+    >
+      <style jsx>{`
+        a {
+            position: absolute;
+            display: inline-block;
+            background-image: url(https://assets.hackclub.com/flag-orpheus-top.svg);
+            background-repeat: no-repeat;
+            background-position: top left;
+            background-size: contain;
+            cursor: pointer;
+            flex-shrink: 0;
+            width: 112px;
+            height: 48px;
+        }
+        @media (min-width: 32em) {
+          a {
+            height: 64px;
+          }
+        }
+        @media (prefers-reduced-motion: no-preference) {
+          a {
+            transition: ${3 / 16}s cubic-bezier(0.375, 0, 0.675, 1) transform;
+            transform-origin: top left;
+          }
+          a:hover,
+          a:focus {
+            animation: waveFlag 0.5s linear infinite alternate;
+          }
+        }
+        @keyframes waveFlag {
+          from {
+            transform: rotate(0deg);
+          }
+          to {
+            transform: rotate(-5deg);
+          }
+        }
+      `}</style>
+    </a>
+  )
+  
+  export default Flag
