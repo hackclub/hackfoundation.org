7a0d87daef206810e923653cdf1c57ed5cc6f322
Sam Poder <39828164+sampoder@users.noreply.github.com>
2021-09-15 19:35:43 +0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
-  darkMode: "class",
   theme: {
     fontFamily: { body: ['"Phantom Sans"'] },
     extend: {},
