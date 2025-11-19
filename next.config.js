7e4966c52918b6e5e22668973c2a1ade44f34469
Parth <215315028+nystar1@users.noreply.github.com>
2025-11-19 18:39:02 +0530

    refactor: no # redirects, optimise assets, use archive.org link

--- next.config.js
+++ next.config.js
@@ -1,2 +1,10 @@
 const withMDX = require('@next/mdx')({ extension: /\.mdx?$/ })
-module.exports = withMDX({ pageExtensions: ['js', 'jsx', 'mdx'] })
+module.exports = withMDX({
+  pageExtensions: ['js', 'jsx', 'mdx'],
+  images: {
+    formats: ['image/avif', 'image/webp'],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    minimumCacheTTL: 60
+  }
+})
