371f72c402b0c03062c13d98cb951232a6619ae5
Echo <github@3kh0.net>
2026-03-23 01:36:16 -0400

    bun it up (#129)

--- next.config.js
+++ next.config.js
@@ -6,6 +6,5 @@ module.exports = withMDX({
     deviceSizes: [384, 640, 828, 1200],
     imageSizes: [256, 384, 512],
     minimumCacheTTL: 3600
-  },
-  swcMinify: true
+  }
 })
--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,14 @@
-import * as React from 'react'
-import NextApp from 'next/app'
 import '@hackclub/theme/fonts/reg-bold.css'
 import theme from '@hackclub/theme'
 import { ThemeProvider } from 'theme-ui'
 
-export default class App extends NextApp {
-  render() {
-    const { Component, pageProps } = this.props
-    return (
-      <ThemeProvider theme={{
-        ...theme,
-        colors: { ...theme.colors, modes: {} }
-      }}>
-        <Component {...pageProps} />
-      </ThemeProvider>
-    )
-  }
+export default function App({ Component, pageProps }) {
+  return (
+    <ThemeProvider theme={{
+      ...theme,
+      colors: { ...theme.colors, modes: {} }
+    }}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
