4d9f8b3309cf2127ac38279c863ed241448f0f60
Echo <github@3kh0.net>
2026-04-01 19:40:03 -0400

    Add vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,20 @@
 import '@hackclub/theme/fonts/reg-bold.css'
 import theme from '@hackclub/theme'
+import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/next"
 import { ThemeUIProvider } from 'theme-ui'
 
 export default function App({ Component, pageProps }) {
   return (
-    <ThemeUIProvider theme={{
-      ...theme,
-      colors: { ...theme.colors, modes: {} }
-    }}>
-      <Component {...pageProps} />
-    </ThemeUIProvider>
+    <>
+      <ThemeUIProvider theme={{
+        ...theme,
+        colors: { ...theme.colors, modes: {} }
+      }}>
+        <Component {...pageProps} />
+      </ThemeUIProvider>
+      <SpeedInsights />
+      <Analytics />
+    </>
   )
 }
